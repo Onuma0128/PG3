9c4b6d54b0be8ae5b1fa79d63dc68fc9611d3bf3
Onuma0128 <147121758+Onuma0128@users.noreply.github.com>
2024-10-05 01:10:04 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,35 +1,44 @@
-#include<stdio.h>
-template <typename T1, typename T2>
+#include <stdio.h>
 
-T1 Min(T1 a, T2 b) {
-	if (a < b) {
-		return a;
-	}
-	return b;
-}
-
-template<>
-char Min<char>(char a, char b) {
-	return printf("数学以外は代入できません\n");
+int Recursive(int n) {
+    if (n == 1) {
+        return 100;
+    }
+    return (2 * Recursive(n - 1) - 50);
 }
 
 int main() {
+    // 働いた時間
+    int n = 10;
+    // 一般的な賃金の合計金額
+    int generalTotalWage = 0;
+    // 再帰的な賃金の合計金額
+    int recursiveTotalWage = 0;
 
-	int int_a = 2;
-	int int_b = 5;
-	printf("%d\n", Min(int_a, int_b));
+    // 各時間ごとの時給を計算して総収入に加算
+    for (int i = 1; i <= n; i++) {
+        int hourlyWage = 1072;
+        generalTotalWage += hourlyWage;
+        printf("一般的な賃金体系\n");
+        printf("%d時間 給料 : %d円\n", i, hourlyWage);
 
-	float float_a = 2.0f;
-	float float_b = 5.0f;
-	printf("%f\n", Min(float_a, float_b));
+        hourlyWage = Recursive(i);
+        recursiveTotalWage += hourlyWage;
+        printf("再帰的な賃金体系\n");
+        printf("%d時間 給料 : %d円\n\n", i, hourlyWage);
+    }
+    // 賃金の合計金額を表示
+    printf("一般的な賃金体系での総収入: %d円\n", generalTotalWage);
+    printf("再帰的な賃金体系での総収入: %d円\n", recursiveTotalWage);
 
-	double double_a = 6.4433f;
-	double double_b = 3.0034f;
-	printf("%lf\n", Min(double_a, double_b));
+    // どちらの金額が上かを表示
+    if (generalTotalWage > recursiveTotalWage) {
+        printf("一般的な賃金体系の方が儲かる");
+    }
+    else {
+        printf("再帰的な賃金体系の方が儲かる");
+    }
 
-	char char_a = 'max';
-	char char_b = 'min';
-	printf("%c\n", Min(char_a, char_b));
 
-	return 0;
+    return 0;
 }
